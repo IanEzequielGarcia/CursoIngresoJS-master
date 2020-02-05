7a6775bd737adc61d20aba097d27fe5b763c092d
Ian G <54332381+Ianchoo@users.noreply.github.com>
2020-02-05 11:55:30 -0300

    ejercicio 9, mal hecho

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,12 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
-
-	
+    var primerNumero;
+    var porcentaje;
+    var resultado;
+    primerNumero=document.getElementById("sueldo").value;
+    parseInt(primerNumero);
+    resultado=parseInt(resultado);
+    resultado=(primerNumero*10)/100+primerNumero;
+    document.getElementById("resultado").value=resultado;
 }
