9f572f6a74fc516536b4feeaff4607e50a2d415d
Ianchoo <54332381+Ianchoo@users.noreply.github.com>
2020-02-05 16:39:48 -0300

    Ej 7 resuelto

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,7 +4,25 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+    var primerNumero;
+    var segundoNumero;
+    var resultado;
+    /* las variables no pueden tener un numero como
+    caracter y no pueden tener espacios*/
+
+    primerNumero=document.getElementById("numeroUno").value;
+    primerNumero=parseInt(primerNumero);
+    console.log("se ingresò: "+primerNumero)
+    segundoNumero=document.getElementById("numeroDos").value;
+    segundoNumero=parseInt(segundoNumero);
+    console.log("se ingresò: "+segundoNumero)
+
+    resultado=primerNumero+segundoNumero;
+    console.log("el resultado es: "+resultado);
+
+
+    // alert no se usa mas a menos que te lo pida el ejercicio
+    // ahora se debe usar console.log
 }
 
 function restar()
@@ -21,4 +39,3 @@ function dividir()
 {
 	
 }
-
