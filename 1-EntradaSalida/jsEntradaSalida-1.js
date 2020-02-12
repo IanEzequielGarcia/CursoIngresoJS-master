e3f0d3f47f861795a92455ae39bacdb9bb5f0ab4
alumno <juan@hotmail.com>
2020-02-12 09:46:23 -0300

    Mi primer commit

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -2,10 +2,12 @@
 function mostrar()
 {
 	// alert("Esto funciona de maravilla");
+	// testeando GIT
 	var nombre1,nombre2;
 	nombre1=prompt("Tirame un nombre");
 	nombre2="Juan";
 	alert(nombre1+" jugo al futbol con "+nombre2);
+
 
 
 
